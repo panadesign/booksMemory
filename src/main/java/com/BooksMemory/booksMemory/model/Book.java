70e5c0ab4c33d7ed11c284ed55d1f0713837d993
Jeremy <charpentier.jeremy@gmail.com>
2023-02-09 18:05:30 +0100

    add lombok

--- src/main/java/com/BooksMemory/booksMemory/model/Book.java
+++ src/main/java/com/BooksMemory/booksMemory/model/Book.java
@@ -1,8 +1,14 @@
 package com.BooksMemory.booksMemory.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,57 +17,6 @@ public class Book {
     private String authorLastname;
     private String authorFirstname;
     private String editor;
-
-    public Book() {
-    }
-
-    public Book(Integer id, String title, String authorLastname, String authorFirstname, String editor) {
-        this.id = id;
-        this.title = title;
-        this.authorLastname = authorLastname;
-        this.authorFirstname = authorFirstname;
-        this.editor = editor;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthorLastname() {
-        return authorLastname;
-    }
-
-    public void setAuthorLastname(String authorLastname) {
-        this.authorLastname = authorLastname;
-    }
-
-    public String getAuthorFirstname() {
-        return authorFirstname;
-    }
-
-    public void setAuthorFirstname(String authorFirstname) {
-        this.authorFirstname = authorFirstname;
-    }
-
-    public String getEditor() {
-        return editor;
-    }
-
-    public void setEditor(String editor) {
-        this.editor = editor;
-    }
 
     public Book update(Book book) {
         this.title = book.getTitle();
